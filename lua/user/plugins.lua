0bfb05612763f1124bad89c2db8b993ae87b6a19
timtursunov <temiki36@gmail.com>
2023-01-23 17:23:52 +0300

    format and add markdown preview

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -57,14 +57,14 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
- -- Telescope
+  -- Telescope
   use "nvim-telescope/telescope.nvim"
   use 'nvim-telescope/telescope-media-files.nvim'
   -- Treesitter
-	use "nvim-treesitter/nvim-treesitter"
+  use "nvim-treesitter/nvim-treesitter"
   -- Autopairs
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-   -- Git
+  -- Git
   use "lewis6991/gitsigns.nvim"
   -- Icons pack
   use "kyazdani42/nvim-web-devicons"
@@ -77,7 +77,7 @@ return packer.startup(function(use)
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
   -- Tabs
-  use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+  use { 'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons' }
   -- project management
   use "ahmedkhalf/project.nvim"
   -- speed up loading lua
@@ -87,27 +87,31 @@ return packer.startup(function(use)
   -- use 'fatih/vim-go'
   -- debug
   use 'mfussenegger/nvim-dap'
-  use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+  use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
   -- use 'leoluz/nvim-dap-go'
   -- floating terminal
   use "akinsho/toggleterm.nvim"
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
 
   use 'simrat39/rust-tools.nvim'
   use 'anuvyklack/keymap-amend.nvim'
   use { 'anuvyklack/fold-preview.nvim',
-   requires = 'anuvyklack/keymap-amend.nvim',
-   config = function()
+    requires = 'anuvyklack/keymap-amend.nvim',
+    config = function()
       require('fold-preview').setup()
-   end
-}
-use { "folke/which-key.nvim" }
-use { 'nvim-telescope/telescope-fzy-native.nvim' }
-use {
+    end
+  }
+  use { "folke/which-key.nvim" }
+  use { 'nvim-telescope/telescope-fzy-native.nvim' }
+  use {
     'numToStr/Comment.nvim',
     config = function()
-        require('Comment').setup()
+      require('Comment').setup()
     end
-}
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -151,6 +151,11 @@ _G.packer_plugins = {
     path = "/Users/timtursunov/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["markdown-preview.nvim"] = {
+    loaded = true,
+    path = "/Users/timtursunov/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim",
+    url = "https://github.com/iamcco/markdown-preview.nvim"
+  },
   ["nvim-autopairs"] = {
     loaded = true,
     path = "/Users/timtursunov/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
@@ -249,14 +254,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: Comment.nvim
-time([[Config for Comment.nvim]], true)
-try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
-time([[Config for Comment.nvim]], false)
 -- Config for: fold-preview.nvim
 time([[Config for fold-preview.nvim]], true)
 try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\17fold-preview\frequire\0", "config", "fold-preview.nvim")
 time([[Config for fold-preview.nvim]], false)
+-- Config for: Comment.nvim
+time([[Config for Comment.nvim]], true)
+try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
+time([[Config for Comment.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
